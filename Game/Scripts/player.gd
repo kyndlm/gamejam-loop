22ed6575cd2b5c4e0acf279c6a206bb2ea99817a
David Wichmann <111539129+davidwchmn@users.noreply.github.com>
2024-11-19 17:57:45 +0100

    AttackAnimation
    Angriff Animaiton hinzugefügt

--- Game/Scripts/player.gd
+++ Game/Scripts/player.gd
@@ -1,23 +1,24 @@
 extends CharacterBody2D
 
 @export var speed: float = 100.0
-
-#animated_sprite ref
 @onready var anim_player = $AnimatedSprite2D
 
 enum State {
 	IDLE,
-	WALK
+	WALK,
+	ATTACK
 }
 
 var current_state: State = State.IDLE
 var last_direction: String = "down"
 
+@export var attack_duration: float = 0.2
+var attack_timer: float = 0.0
+var is_attacking: bool = false
 
 func _physics_process(delta: float) -> void:
 	var direction = Vector2.ZERO
 
-	# Detect input for movement
 	if Input.is_action_pressed("right"):
 		direction.x += 1
 		anim_player.flip_h = false
@@ -28,24 +29,35 @@ func _physics_process(delta: float) -> void:
 		direction.y += 1
 	if Input.is_action_pressed("up"):
 		direction.y -= 1
-		
+
 	direction = direction.normalized()
 
-	#Update state:
-	if direction == Vector2.ZERO:
+	if is_attacking:
+		attack_timer -= delta
+		if attack_timer <= 0:
+			is_attacking = false
+			current_state = State.IDLE
+	elif Input.is_action_just_pressed("attack"):
+		current_state = State.ATTACK
+		is_attacking = true
+		attack_timer = attack_duration
+	elif direction == Vector2.ZERO:
 		current_state = State.IDLE
 	else:
 		current_state = State.WALK
-		
+
 	match current_state:
 		State.IDLE:
 			_handle_idle_state()
 		State.WALK:
 			_handle_walk_state(direction)
-		
-	velocity = direction * speed
-	move_and_slide()
-	
+		State.ATTACK:
+			_handle_attack_state()
+
+	if current_state != State.ATTACK:
+		velocity = direction * speed
+		move_and_slide()
+
 func _handle_idle_state() -> void:
 	match last_direction:
 		"down":
@@ -54,7 +66,7 @@ func _handle_idle_state() -> void:
 			anim_player.play("idle_up")
 		"side":
 			anim_player.play("idle_side")
-			
+
 func _handle_walk_state(direction: Vector2) -> void:
 	if direction.y > 0:
 		last_direction = "down"
@@ -65,3 +77,12 @@ func _handle_walk_state(direction: Vector2) -> void:
 	else:
 		last_direction = "side"
 		anim_player.play("walk_side")
+
+func _handle_attack_state() -> void:
+	match last_direction:
+		"down":
+			anim_player.play("attack_down")
+		"up":
+			anim_player.play("attack_up")
+		"side":
+			anim_player.play("attack_side")
