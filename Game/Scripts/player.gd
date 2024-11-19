d2039d17dc168bc874482a207f01724a8703400e
David Wichmann <111539129+davidwchmn@users.noreply.github.com>
2024-11-19 14:25:53 +0100

    Basic Player Movement

--- Game/Scripts/player.gd
+++ Game/Scripts/player.gd
@@ -1,28 +1,21 @@
 extends CharacterBody2D
 
+@export var speed: float = 200.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _physics_process(delta: float) -> void:
+	var direction = Vector2.ZERO
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+	# Detect input for movement
+	if Input.is_action_pressed("right"):
+		direction.x += 1
+	if Input.is_action_pressed("left"):
+		direction.x -= 1
+	if Input.is_action_pressed("down"):
+		direction.y += 1
+	if Input.is_action_pressed("up"):
+		direction.y -= 1
+		
+	direction = direction.normalized()
 
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	velocity = direction * speed
 	move_and_slide()
