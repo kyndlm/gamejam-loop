07a9dc54e286c24c292d7acbb19f6aadd90cd876
David Wichmann <111539129+davidwchmn@users.noreply.github.com>
2024-11-19 16:41:43 +0100

    Updated Player Sprite
    neue player animations

--- Game/Scripts/player.gd
+++ Game/Scripts/player.gd
@@ -1,6 +1,18 @@
 extends CharacterBody2D
 
-@export var speed: float = 200.0
+@export var speed: float = 100.0
+
+#animated_sprite ref
+@onready var anim_player = $AnimatedSprite2D
+
+enum State {
+	IDLE,
+	WALK
+}
+
+var current_state: State = State.IDLE
+var last_direction: String = "down"
+
 
 func _physics_process(delta: float) -> void:
 	var direction = Vector2.ZERO
@@ -8,8 +20,10 @@ func _physics_process(delta: float) -> void:
 	# Detect input for movement
 	if Input.is_action_pressed("right"):
 		direction.x += 1
+		anim_player.flip_h = false
 	if Input.is_action_pressed("left"):
 		direction.x -= 1
+		anim_player.flip_h = true
 	if Input.is_action_pressed("down"):
 		direction.y += 1
 	if Input.is_action_pressed("up"):
@@ -17,5 +31,37 @@ func _physics_process(delta: float) -> void:
 		
 	direction = direction.normalized()
 
+	#Update state:
+	if direction == Vector2.ZERO:
+		current_state = State.IDLE
+	else:
+		current_state = State.WALK
+		
+	match current_state:
+		State.IDLE:
+			_handle_idle_state()
+		State.WALK:
+			_handle_walk_state(direction)
+		
 	velocity = direction * speed
 	move_and_slide()
+	
+func _handle_idle_state() -> void:
+	match last_direction:
+		"down":
+			anim_player.play("idle_down")
+		"up":
+			anim_player.play("idle_up")
+		"side":
+			anim_player.play("idle_side")
+			
+func _handle_walk_state(direction: Vector2) -> void:
+	if direction.y > 0:
+		last_direction = "down"
+		anim_player.play("walk_down")
+	elif direction.y < 0:
+		last_direction = "up"
+		anim_player.play("walk_up")
+	else:
+		last_direction = "side"
+		anim_player.play("walk_side")
