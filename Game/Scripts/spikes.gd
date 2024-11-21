f9d885681a3f1948ef06faf6db7deb80792879c9
Kiyan do Lago Martins <74674163+kyndlm@users.noreply.github.com>
2024-11-21 15:29:12 +0100

    spikes final mit einem bug david meinte nein es reicht

--- Game/Scripts/spikes.gd
+++ Game/Scripts/spikes.gd
@@ -1,17 +1,22 @@
 extends Area2D
 
 var player_in_area: bool = false
-
+var damage_interval: float = 1.0  # Damage interval (1 second)
+var last_damage_time: float = 0.0  # Time when the last damage was applied
+var area1: Area2D
 func _physics_process(delta: float) -> void:
-	if player_in_area == true:
-		GameManager.playerHit()
-		print("damage")
-		await get_tree().create_timer(50).timeout
-
+	# Check if player is in area and enough time has passed for the next damage
+	if player_in_area and (Time.get_ticks_msec() / 1000.0 - last_damage_time >= damage_interval):
+		GameManager.playerHit()  # Apply damage
+		print("Damage applied")
+		last_damage_time = Time.get_ticks_msec() / 1000.0  # Update the last damage time
 
 func _on_area_entered(area: Area2D) -> void:
+	area1 = area
+	print(area)
 	player_in_area = true
 
 
 func _on_area_exited(area: Area2D) -> void:
+	area1 = area
 	player_in_area = false
